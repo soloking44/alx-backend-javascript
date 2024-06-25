2b674ea0bea8c7739eedf2540d9ffedc1e5b5637
soloking44 <solop8483@gmail.com>
2024-06-25 17:26:59 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,31 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
+function handleProfileSignup(
   firstName,
   lastName,
   fileName,
 ) {
-  const res = [];
-  try {
-    const user = await signUpUser(firstName, lastName);
-    res.push({ status: 'fulfilled', value: user });
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: err.toString(),
-    });
-  }
-
-  try {
-    const upload = await uploadPhoto(fileName);
-    res.push({ status: 'fulfilled', value: upload });
-  } catch (err) {
-    res.push({
-      status: 'rejected',
-      value: err.toString(),
-    });
-  }
-
-  return res;
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((response) => response.map((res) => ({
+    status: res.status,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
+  })));
 }
+
+export default handleProfileSignup;
