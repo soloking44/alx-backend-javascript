e791ff0617749d39d315b60de0d06197432d8087
soloking44 <solop8483@gmail.com>
2024-06-27 12:03:54 +0000

    8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,36 +1,20 @@
 export default class HolbertonClass {
   constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
     this._size = size;
     this._location = location;
   }
 
-  get size() {
-    return this._size;
-  }
-
-  set size(newSize) {
-    if (typeof newSize !== 'number') {
-      throw new TypeError('Size must be a number');
-    }
-    this._size = newSize;
-  }
-
-  get location() {
+  toString() {
     return this._location;
-  }
-
-  set location(newLocation) {
-    if (typeof newLocation !== 'string') {
-      throw new TypeError('Location must be a string');
-    }
-    this._location = newLocation;
   }
 
   valueOf() {
     return this._size;
-  }
-
-  toString() {
-    return this._location;
   }
 }
