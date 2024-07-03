e64279edaf413de26b30fe1dc3ca77efdefa94a9
soloking44 <solop8483@gmail.com>
2024-07-03 12:28:07 +0000

    advance typed part 1

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,42 +1,38 @@
 interface DirectorInterface {
-    workFromHome(): string,
-    getCoffeeBreak(): string,
-    workDirectorTasks(): string
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
-    workFromHome(): string,
-    getCoffeeBreak(): string,
-    workTeacherTasks(): string
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
 }
-
+  
 class Director implements DirectorInterface {
-    workFromHome(): string {
-        return "Working from home"
-    }
-    getCoffeeBreak(): string {
-        return "Getting a coffee break"
-    }
-    workDirectorTasks(): string {
-        return "Getting to director tasks"
-    }
+  workFromHome = () : string => 'Working from home';
+  getCoffeeBreak = () : string => 'Getting a coffee break';
+  workDirectorTasks = () : string => 'Getting to director tasks';
 }
 
 class Teacher implements TeacherInterface {
-    workFromHome(): string {
-        return "Cannot work from home"
-    }
-    getCoffeeBreak(): string {
-        return "Cannot have a break"
-    }
-    workTeacherTasks(): string {
-        return "Getting to work"
-    }
+  workFromHome = () : string => 'Cannot work from home';
+  getCoffeeBreak = () : string => 'Cannot have a break';
+  workTeacherTasks = () : string => 'Getting to work';
 }
 
-function createEmployee(salary: number | string) {
-    if (typeof salary === "number" && salary < 500) {
-        return Teacher
-    }
-    return Director
+export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
+
+export const executeWork = (employee: Teacher | Director): void => {
+  const work = employee instanceof Teacher? employee.workTeacherTasks() : employee.workDirectorTasks();
+  console.log(work)
+}
+
+type Subjects = 'Math' | 'History';
+
+export const teachClass = (todayClass: Subjects) : string => {
+  return todayClass === 'Math' ? 'Teaching Math': 'Teaching History';
 }
